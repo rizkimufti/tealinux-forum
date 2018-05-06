0ce572a4141dbac69e266af83592002d2f34cb79
rizkimufti <rizkimufti@gmail.com>
2018-05-06 23:06:48 +0700

    config

---
+++ config.php
@@ -0,0 +1,22 @@
+<?php return array (
+  'debug' => true,
+  'database' =>
+  array (
+    'driver' => 'mysql',
+    'host' => 'localhost',
+    'database' => 'fixforum',
+    'username' => 'root',
+    'password' => '',
+    'charset' => 'utf8mb4',
+    'collation' => 'utf8mb4_unicode_ci',
+    'prefix' => 'tea_',
+    'port' => '3306',
+    'strict' => false,
+  ),
+  'url' => 'http://localhost/flarum',
+  'paths' =>
+  array (
+    'api' => 'api',
+    'admin' => 'admin',
+  ),
+);
